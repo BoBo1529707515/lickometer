2ec3bba16581b00bd3ad079d99a587fcda20027c
一百元的彼岸(bobo) <145309276+BoBo1529707515@users.noreply.github.com>
2025-09-28 16:46:38 +0800

    Update bsp_touchkey.h

--- UARTNEW/MDK-ARM/bsp_touchkey.h
+++ UARTNEW/MDK-ARM/bsp_touchkey.h
@@ -1,16 +1,16 @@
 #ifndef __BSP_TOUCHKEY_H
 #define __BSP_TOUCHKEY_H
 
-#include "stm32f1xx_hal.h"  // ??HAL????
+#include "stm32f1xx_hal.h"  
 
-// ????????(????????)
+
 #define TOUCH_PORT        GPIOA
 #define TOUCH_PIN_1       GPIO_PIN_1
 #define TOUCH_PIN_2       GPIO_PIN_2
 #define TOUCH_PIN_3       GPIO_PIN_3
 #define TOUCH_PIN_4       GPIO_PIN_4
 
-// ????
+
 void TTP224_GPIO_Init(void);
 uint8_t Key_IN1_Scanf(void);
 uint8_t Key_IN2_Scanf(void);
